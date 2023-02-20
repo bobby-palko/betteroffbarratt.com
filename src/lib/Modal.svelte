72f8963db9edc989eeaaf3b6e97510fc94c303dc
Bobby Palko <bobby@palko.dev>
2023-02-20 16:01:31 -0500

    more modal tweaks

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -61,11 +61,11 @@
 
 <style>
     .modal {
-        position: fixed;
+        position: absolute;
         top: 0;
         left: 0;
-        width: 100%;
-        height: 100%;
+        width: 100vw;
+        height: 100vh;
         z-index: 99;
         background-color: var(--cream);
     }
